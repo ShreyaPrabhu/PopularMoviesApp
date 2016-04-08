70081d7280d6a6cc6b229b933f5c6569b9bf2fc7
ShreyaPrabhu <shreyaprabhu96@gmail.com>
2016-04-08 16:20:47 +0530

    Image library and API used to list movies

--- app/src/main/java/com/example/shreyaprabhu/popularmovies/MovieMainFragment.java
+++ app/src/main/java/com/example/shreyaprabhu/popularmovies/MovieMainFragment.java
@@ -1,10 +1,15 @@
 package com.example.shreyaprabhu.popularmovies;
 
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -12,16 +17,34 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 //Fragment activity has Movielist
 
 public class MovieMainFragment extends Fragment {
 
 
     private android.support.v7.widget.Toolbar toolbar;
+    private RecyclerView mRecyclerView;
+    private List<MovieAttributes> mMovies = new ArrayList<>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        fetchMovieDetails fetchMovieDetails = new fetchMovieDetails();
+        fetchMovieDetails.execute();
 
     }
 
@@ -34,6 +57,14 @@ public class MovieMainFragment extends Fragment {
         //add custom toolbar
         toolbar = (Toolbar) view.findViewById(R.id.app_bar);
         ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.fragment_movie_main_recyclerview);
+        mRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+
+
+        if (isAdded()) {
+            mRecyclerView.setAdapter(new MovieRecyclerAdapter(getActivity(), mMovies));
+        }
 
 
         return view;
@@ -62,4 +93,98 @@ public class MovieMainFragment extends Fragment {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private class fetchMovieDetails extends AsyncTask<Void, Void, List<MovieAttributes>> {
+
+
+        private final String TAG = fetchMovieDetails.class.getSimpleName();
+        private final String KEY ="";  //here goes the api key
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(List<MovieAttributes> movies) {
+            super.onPostExecute(movies);
+            mMovies = movies;
+
+            if (isAdded()) {
+                mRecyclerView.setAdapter(new MovieRecyclerAdapter(getActivity(), mMovies));
+            }
+
+        }
+
+        @Override
+        protected List<MovieAttributes> doInBackground(Void... params) {
+
+
+            List<MovieAttributes> movies = new ArrayList<>();
+
+            try {
+                OkHttpClient client = new OkHttpClient();
+
+                // Build URL
+                String url = Uri.parse("http://api.themoviedb.org/3/movie/popular")
+                        .buildUpon()
+                        .appendQueryParameter("api_key", KEY)
+                        .appendQueryParameter("page", "1")
+                        .build().toString();
+
+
+                // Fetch Data from URL
+                Request request = new Request.Builder()
+                        .url(url)
+                        .build();
+
+                Call call = client.newCall(request);
+                Response response = call.execute();
+                if (!response.isSuccessful()) {
+                    throw new IOException("Err Code: " + response);
+                }
+                String responseStr = response.body().string();
+                Log.v(TAG, "Response Str: " + responseStr);
+                movies = fetchMovieData(responseStr);
+
+            } catch (IOException e) {
+                Log.e(TAG, "Failed to Fetch Data: ", e);
+            }
+            return movies;
+        }
+
+
+    }
+
+
+    public static List<MovieAttributes> fetchMovieData(String jsonStr) {
+
+
+        List<MovieAttributes> movies = new ArrayList<>();
+
+        try {
+
+            JSONObject jsonObject = new JSONObject(jsonStr);
+            JSONArray jsonArray = jsonObject.getJSONArray("results");
+
+
+            for (int i = 0; i < jsonArray.length(); ++i) {
+                JSONObject movieJson = jsonArray.getJSONObject(i);
+                MovieAttributes movie = new MovieAttributes();
+                movie.setTitle(movieJson.getString("title"));
+                movie.setposterPath(movieJson.getString("poster_path"));
+                movie.setoverview(movieJson.getString("overview"));
+                movie.setreleaseDate(movieJson.getString("release_date"));
+                movie.setrating(movieJson.getString("vote_average"));
+                movies.add(movie);
+            }
+        } catch (JSONException e) {
+            Log.e("JsonException", e.getMessage(), e);
+            e.printStackTrace();
+
+        }
+        return movies;
+    }
+
 }
--- app/src/main/java/com/example/shreyaprabhu/popularmovies/MovieRecyclerAdapter.java
+++ app/src/main/java/com/example/shreyaprabhu/popularmovies/MovieRecyclerAdapter.java
@@ -2,8 +2,12 @@ package com.example.shreyaprabhu.popularmovies;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -16,14 +20,17 @@ public class MovieRecyclerAdapter extends RecyclerView.Adapter<MovieRecyclerAdap
 
     private List<MovieAttributes> moviesList;
     private static Context mContext;
-    private MovieAttributes mMovies;
+    private MovieAttributes mMovie;
 
 
     public static class MovieItemViewHolder extends RecyclerView.ViewHolder {
 
+        private ImageView mImageView;
+
         public MovieItemViewHolder(View itemView) {
             super(itemView);
-
+            mImageView = (ImageView) itemView.findViewById(R.id.imageView);
+            //set onitemClickListener for each movie_item
         }
     }
     public MovieRecyclerAdapter(Context context, List<MovieAttributes> movies) {
@@ -37,16 +44,24 @@ public class MovieRecyclerAdapter extends RecyclerView.Adapter<MovieRecyclerAdap
     @Override
     public MovieItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
-        return null;
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.movie_item, parent, false);
+        return new MovieItemViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(MovieItemViewHolder holder, int position) {
+
+        mMovie = moviesList.get(position);
+        String posterUrl = mMovie.getposterPath();
+        Picasso.with(mContext).load("http://image.tmdb.org/t/p/w500" + posterUrl).placeholder(R.drawable.placeholder).into((holder).mImageView);
+
+
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return moviesList.size();
     }
 
 
